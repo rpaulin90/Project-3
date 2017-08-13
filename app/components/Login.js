3d39854a754e74009269260995e1edcaa6fe14d3
Ricardo Paulin <rpaulin@ricardos-mbp.attlocal.net>
2017-08-13 11:02:32 -0500

    fixed scope issues in uploadFileUser

--- app/components/Login.js
+++ app/components/Login.js
@@ -85,12 +85,13 @@ class Login extends Component {
     }
     getSignedRequestUser(file){
         const xhr = new XMLHttpRequest();
+        let that = this;
         xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
         xhr.onreadystatechange = function() {
             if(xhr.readyState === 4){
                 if(xhr.status === 200){
                     const response = JSON.parse(xhr.responseText);
-                    this.uploadFileUser(file, response.signedRequest, response.url);
+                    that.uploadFileUser(file, response.signedRequest, response.url);
                 }
                 else{
                     alert('Could not get signed URL.');
@@ -101,11 +102,12 @@ class Login extends Component {
     }
     uploadFileUser(file, signedRequest, url){
         const xhr = new XMLHttpRequest();
+        let that = this;
         xhr.open('PUT', signedRequest);
         xhr.onreadystatechange = () => {
             if(xhr.readyState === 4){
                 if(xhr.status === 200){
-                    this.setState({ profilePic: url });
+                    that.setState({ profilePic: url });
                     //document.getElementById('preview').src = url;
                     //document.getElementById('avatar-url').value = url;
                 }
